f3869b5f03a144440ae5fc93e6fa19c4841a1180
WahyuSeptiadi <arstuw@gmail.com>
2021-01-05 09:29:35 +0700

    prepare clean architecture

--- app/src/main/java/com/codeart/filmskuy/home/MainActivity.kt
+++ app/src/main/java/com/codeart/filmskuy/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.codeart.filmskuy
+package com.codeart.filmskuy.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codeart.filmskuy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
