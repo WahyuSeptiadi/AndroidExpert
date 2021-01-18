6a620fb163ef3539114a906605b1edfc9a228a1e
WahyuSeptiadi <wahyusptd@gmail.com>
2021-01-18 17:36:52 +0700

    cleanup unused code

--- core/src/main/java/com/codeart/filmskuy/core/utils/AppExecutors.kt
+++ core/src/main/java/com/codeart/filmskuy/core/utils/AppExecutors.kt
@@ -1,7 +1,5 @@
 package com.codeart.filmskuy.core.utils
 
-import android.os.Handler
-import android.os.Looper
 import androidx.annotation.VisibleForTesting
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
@@ -12,31 +10,11 @@ import java.util.concurrent.Executors
  */
 
 class AppExecutors @VisibleForTesting constructor(
-    private val diskIO: Executor,
-    private val networkIO: Executor,
-    private val mainThread: Executor
+    private val diskIO: Executor
 ){
-    companion object {
-        private const val THREAD_COUNT = 3
-    }
-
     constructor() : this(
-        Executors.newSingleThreadExecutor(),
-        Executors.newFixedThreadPool(THREAD_COUNT),
-        MainThreadExecutor()
+        Executors.newSingleThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
-
-    fun networkIO(): Executor = networkIO
-
-    fun mainThread(): Executor = mainThread
-
-    private class MainThreadExecutor : Executor {
-        private val mainThreadHandler = Handler(Looper.getMainLooper())
-
-        override fun execute(command: Runnable) {
-            mainThreadHandler.post(command)
-        }
-    }
 }
