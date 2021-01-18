ba99b571b03e52948a9b6a6771ede1d5d2465f9a
WahyuSeptiadi <wahyusptd@gmail.com>
2021-01-18 20:55:22 +0700

    add new file from new-feature

---
+++ favorite/build/tmp/kapt3/stubs/debug/com/codeart/filmskuy/favorite/MyFavoritePagerAdapter.java
@@ -0,0 +1,32 @@
+package com.codeart.filmskuy.favorite;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 1, d1 = {"\u00000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0002\b\u0003\n\u0002\u0010\r\n\u0000\u0018\u00002\u00020\u0001B\u0015\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\u0006J\b\u0010\n\u001a\u00020\u000bH\u0016J\u0010\u0010\f\u001a\u00020\t2\u0006\u0010\r\u001a\u00020\u000bH\u0016J\u0010\u0010\u000e\u001a\u00020\u000f2\u0006\u0010\r\u001a\u00020\u000bH\u0016R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0007\u001a\b\u0012\u0004\u0012\u00020\t0\bX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0010"}, d2 = {"Lcom/codeart/filmskuy/favorite/MyFavoritePagerAdapter;", "Landroidx/fragment/app/FragmentPagerAdapter;", "context", "Landroid/content/Context;", "fm", "Landroidx/fragment/app/FragmentManager;", "(Landroid/content/Context;Landroidx/fragment/app/FragmentManager;)V", "pages", "", "Landroidx/fragment/app/Fragment;", "getCount", "", "getItem", "position", "getPageTitle", "", "favorite_debug"})
+public final class MyFavoritePagerAdapter extends androidx.fragment.app.FragmentPagerAdapter {
+    private final java.util.List<androidx.fragment.app.Fragment> pages = null;
+    private final android.content.Context context = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public androidx.fragment.app.Fragment getItem(int position) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public int getCount() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.CharSequence getPageTitle(int position) {
+        return null;
+    }
+    
+    public MyFavoritePagerAdapter(@org.jetbrains.annotations.NotNull()
+    android.content.Context context, @org.jetbrains.annotations.NotNull()
+    androidx.fragment.app.FragmentManager fm) {
+        super(null);
+    }
+}
---
+++ favorite/build/tmp/kapt3/stubs/debug/com/codeart/filmskuy/favorite/tvshow/TvShowFavoriteViewModel.java
@@ -0,0 +1,23 @@
+package com.codeart.filmskuy.favorite.tvshow;
+
+import java.lang.System;
+
+/**
+ * Created by wahyu_septiadi on 18, January 2021.
+ * Visit My GitHub --> https://github.com/WahyuSeptiadi
+ */
+@kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004R\u001d\u0010\u0005\u001a\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\b0\u00070\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u00a8\u0006\u000b"}, d2 = {"Lcom/codeart/filmskuy/favorite/tvshow/TvShowFavoriteViewModel;", "Landroidx/lifecycle/ViewModel;", "catalogueUseCase", "Lcom/codeart/filmskuy/core/domain/usecase/CatalogueUsecase;", "(Lcom/codeart/filmskuy/core/domain/usecase/CatalogueUsecase;)V", "tvShowFavorite", "Landroidx/lifecycle/LiveData;", "", "Lcom/codeart/filmskuy/core/domain/model/CatalogueModel;", "getTvShowFavorite", "()Landroidx/lifecycle/LiveData;", "favorite_debug"})
+public final class TvShowFavoriteViewModel extends androidx.lifecycle.ViewModel {
+    @org.jetbrains.annotations.NotNull()
+    private final androidx.lifecycle.LiveData<java.util.List<com.codeart.filmskuy.core.domain.model.CatalogueModel>> tvShowFavorite = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.LiveData<java.util.List<com.codeart.filmskuy.core.domain.model.CatalogueModel>> getTvShowFavorite() {
+        return null;
+    }
+    
+    public TvShowFavoriteViewModel(@org.jetbrains.annotations.NotNull()
+    com.codeart.filmskuy.core.domain.usecase.CatalogueUsecase catalogueUseCase) {
+        super();
+    }
+}
