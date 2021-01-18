aa81cfc68224ed88a561ffcb992e8b7e678b851c
WahyuSeptiadi <wahyusptd@gmail.com>
2021-01-18 09:53:02 +0700

    delete favorite activity hehe

--- app/src/main/java/com/codeart/filmskuy/MainActivity.kt
+++ app/src/main/java/com/codeart/filmskuy/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.codeart.filmskuy
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.codeart.filmskuy.core.ui.MyPagerAdapter
 import com.codeart.filmskuy.databinding.ActivityMainBinding
-import com.codeart.filmskuy.favorite.FavoriteActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,10 +16,5 @@ class MainActivity : AppCompatActivity() {
 
         binding.viewpagerMain.adapter = MyPagerAdapter(this, supportFragmentManager)
         binding.tabsMain.setupWithViewPager(binding.viewpagerMain)
-
-        binding.favorite.setOnClickListener {
-            val intent = Intent(this, FavoriteActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
